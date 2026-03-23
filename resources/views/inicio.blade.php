e91293534d5d88581d31cd506cfd0d5dec5c2440
Vanesa_guazaquillo <Vanessa.Guazaquillo@utp.edu.co>
2026-03-23 15:43:03 -0500

    archivo

--- resources/views/inicio.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title','Inicio')
-
-@section('content')
-    <h1>Inicio</h1>
-    <p>Este es el menu principal.Prueba los enlaces de arriba,</p>
-    @endsection
